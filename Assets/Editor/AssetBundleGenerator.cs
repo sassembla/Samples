2f483ee2a9507a67627fe6349436d35fcd047e82
Toru <toru.inoue@kissaki.tv>
2014-12-18 12:32:27 +0900

    add Resources & AssetBundleGenerator

---
+++ Assets/Editor/AssetBundleGenerator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System;
+using System.IO;
+using System.Collections;
+
+public class AssetBundleGenerator {
+	/*
+		Resourcesから取得できるResource名(ファイル自体はjpg)
+	*/
+	static string bundleResourceName1 = "sushi";
+	static string bundleResourceName2 = "udon";
+
+	// AssetBundleになったときの名前
+	static string bundleName = "SampleAssetBundle.unity3d";
+
+	// AssetBundle化したファイルを置く場所
+	static string outputBasePath = "bundlized";
+
+	// AssetBundleをDownloadするためのurl
+	static string url = Path.Combine("file:///", bundleName);
+
+	
+	[MenuItem ("Window/Generate AssetBundle", false, 1)]
+	static void GenerateAssetBundle () {
+		/*
+			assetBundleResource1, assetBundleResource2に、
+			ResourcesからResourceを読み出す
+		*/
+		var assetBundleResource1 = Resources.Load(bundleResourceName1);
+		if (assetBundleResource1 != null) {
+			Debug.Log("assetBundleResource1:" + assetBundleResource1.name);
+		}
+
+		var assetBundleResource2 = Resources.Load(bundleResourceName2);
+		if (assetBundleResource2 != null) {
+			Debug.Log("assetBundleResource2:" + assetBundleResource2.name);
+		}
+
+
+
+		// ターゲットプラットフォームの設定(現在のエディタの環境のものを読み込んで使用しています)
+		BuildTarget targetPlatform = EditorUserBuildSettings.activeBuildTarget;
+
+
+		// AssetBundleそれ自体のファイル名を加えたパス(この場合、PROJECT_FOLDER/outputBasePath/bundleName)
+		var assetBundleOutputPath = Path.Combine(outputBasePath, bundleName);
+
+		uint crc;
+
+		// assetBundleResource1, assetBundleResource2 を含んだAssetBundleを、assetBundleOutputPathに出力します
+		BuildPipeline.BuildAssetBundle(
+			assetBundleResource1,
+			new UnityEngine.Object[]{assetBundleResource2},
+			assetBundleOutputPath,
+			out crc,
+			BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets,
+			targetPlatform
+		);
+
+		Debug.Log("crc:" + crc);
+
+		if (File.Exists(assetBundleOutputPath)) {
+			Debug.Log("AssetBundle generated:" + assetBundleOutputPath);
+		} else {
+			Debug.Log("failed to generate AssetBundle.");
+		}
+		
+	}
+}
--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -1,16 +1,66 @@
 ﻿using UnityEngine;
+using UnityEditor;
+
 using System;
 using System.IO;
 using System.Collections;
 
 public class Loader : MonoBehaviour {
+	/*
+		Resourcesから取得できるResource名(ファイル自体はjpg)
+	*/
+	static string bundleResourceName1 = "sushi";
+	static string bundleResourceName2 = "udon";
 
-	static string bundleResourceName1 = "";
+	// AssetBundleになったときの名前
 	static string bundleName = "SampleAssetBundle.unity3d";
-	static string url = Path.Combine("file:///AssetRails/temp/Resources/bundlize/", bundleName);
 
+	// AssetBundle化したファイルを置く場所
+	static string outputBasePath = "bundlized";
+
+	// AssetBundleをDownloadするためのurl
+	static string url = Path.Combine("file:///", bundleName);
+
+	
+
+	
 	void GenerateAssetBundle () {
-		// var assetBundleResource = Resources.bu
+
+		/*
+			assetBundleResource1, assetBundleResource2に、
+			ResourcesからResourceを読み出す
+		*/
+		var assetBundleResource1 = Resources.Load(bundleResourceName1);
+		if (assetBundleResource1 != null) {
+			Debug.Log("assetBundleResource1:" + assetBundleResource1.name);
+		}
+
+		var assetBundleResource2 = Resources.Load(bundleResourceName2);
+		if (assetBundleResource2 != null) {
+			Debug.Log("assetBundleResource2:" + assetBundleResource2.name);
+		}
+
+
+
+		// ターゲットプラットフォームの設定(現在のエディタの環境のものを読み込んで使用しています)
+		BuildTarget targetPlatform = EditorUserBuildSettings.activeBuildTarget;
+
+
+		// AssetBundleそれ自体のファイル名を加えたパス(この場合、PROJECT_FOLDER/outputBasePath/bundleName)
+		var assetBundleOutputPath = Path.Combine(outputBasePath, bundleName);
+
+		uint crc;
+
+		BuildPipeline.BuildAssetBundle(
+			assetBundleResource1,
+			new UnityEngine.Object[]{assetBundleResource2},
+			assetBundleOutputPath,
+			out crc,
+			BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets,
+			targetPlatform
+		);
+
+		Debug.Log("crc:" + crc);
 	}
 
 
@@ -109,6 +159,7 @@ public class Loader : MonoBehaviour {
 			}
 
 			default: {
+				Debug.Log("cacheState:" + cacheState);
 				break;
 			}
 		}
