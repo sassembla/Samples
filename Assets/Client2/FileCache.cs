012863003b7f2d914fe368a8ca8c1e8b46b110d0
Toru <toru.inoue@kissaki.tv>
2014-12-21 12:41:14 +0900

    delete ニホンゴ = コメント

--- Assets/Client2/FileCache.cs
+++ Assets/Client2/FileCache.cs
@@ -7,7 +7,7 @@ public class FileCache {
 
 	public static string UpdateList (string data) {
 		Debug.Log("UpdateList:" + data);
-		Debug.Log("起動するたび消すのが面倒くさいので特に何もしません、保存したことにして、そのまま入力データを返します。");
+		// 起動するたび消すのが面倒くさいので特に何もしません、保存したことにして、そのまま入力データを返します。
 		return data;
 	}
 }
